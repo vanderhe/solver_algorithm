a28431398816cf9e19e667bd3e8c39f781b3e162
Tammo van der Heide <vanderhe@uni-bremen.de>
2018-06-12 09:47:59 +0200

    """Implementing linear-dependency-detection with numpy function linalg.det()."""

--- solvers.py
+++ solvers.py
@@ -13,38 +13,46 @@ def gaussian_eliminate(aa, bb):
         Vector xx with the solution of the linear equation or None
         if the equations are linearly dependent.
     """
-    
+
     aa_rows = aa.shape[0]
     bb_rows = aa_rows
     aa_columns = aa.shape[1]
-    
-    #Transformation to the lower-triangular-shape form:
-    for ii in range(0, aa_rows, 1):
-        for jj in range(ii + 1, aa_columns, 1):
-            if aa[ii, ii] != 0:
-                var = (aa[jj, ii] / aa[ii, ii])
-                aa[jj, ii:] = aa[jj, ii:] - var * aa[ii, ii:]
-                bb[jj] = bb[jj] - var * bb[ii]
-            elif aa[ii, ii] == 0:
-                switch_var = np.array(aa[ii, :])
-                aa[ii, :] = aa[jj, :]
-                aa[jj, :] = switch_var
-    
-    #Stack aa and bb to one matrix:
-    bb_shape = bb.reshape((-1,1))
-    sol = np.hstack([aa, bb_shape])
-    
-    for ii in range(0, aa_rows, 1):
-        sol[ii, :] = sol[ii, :] / sol[ii, ii]
-    
+
+    #Test for linear dependency:
+    det = np.linalg.det(aa)
+    if abs(det) > 5*10**(-13):
+
+        #Transformation to the lower-triangular-shape form:
+        for ii in range(0, aa_rows, 1):
+            for jj in range(ii + 1, aa_columns, 1):
+                if aa[ii, ii] != 0:
+                    var = (aa[jj, ii] / aa[ii, ii])
+                    aa[jj, ii:] = aa[jj, ii:] - var * aa[ii, ii:]
+                    bb[jj] = bb[jj] - var * bb[ii]
+                elif aa[ii, ii] == 0:
+                    switch_var = np.array(aa[ii, :])
+                    aa[ii, :] = aa[jj, :]
+                    aa[jj, :] = switch_var
+
+        #Stack aa and bb to one matrix:
+        bb_shape = bb.reshape((-1,1))
+        sol = np.hstack([aa, bb_shape])
+
+        for ii in range(0, aa_rows, 1):
+            sol[ii, :] = sol[ii, :] / sol[ii, ii]
+
     #print("lower-triangular-shape form:\n", sol)
-    
-    #Transformation to the final form:        
-    for ii in range(0, aa_rows, 1):
-        for kk in range(ii + 1, aa_columns, 1):
-            sol[ii, :] = sol[ii, :] - sol[ii, kk] * sol[kk, :]
-    sol_bb = sol[:, aa_columns]
-    
-    #print("\nfinal form:\n", sol)    
-    
+
+        #Transformation to the final form:
+        for ii in range(0, aa_rows, 1):
+            for kk in range(ii + 1, aa_columns, 1):
+                sol[ii, :] = sol[ii, :] - sol[ii, kk] * sol[kk, :]
+                sol_bb = sol[:, aa_columns]
+
+    else:
+        print("linear dependency detected!")
+        sol_bb = None
+
+    #print("\nfinal form:\n", sol)
+
     return sol_bb
