7e381e76e6509d90e5091b42f279f4a77124427b
Tammo van der Heide <vanderhe@uni-bremen.de>
2018-06-19 11:19:53 +0200

    """Migration to Spyder3. Input and Output working."""

--- solvers.py
+++ solvers.py
@@ -1,7 +1,6 @@
 """Routines for solving a linear system of equations."""
 import numpy as np
 
-
 def gaussian_eliminate(aa, bb):
     """Solves a linear system of equations (Ax = b) by Gauss-elimination
 
@@ -41,18 +40,56 @@ def gaussian_eliminate(aa, bb):
         for ii in range(0, aa_rows, 1):
             sol[ii, :] = sol[ii, :] / sol[ii, ii]
 
-    #print("lower-triangular-shape form:\n", sol)
+        print("lower-triangular-shape form:\n", sol)
 
         #Transformation to the final form:
         for ii in range(0, aa_rows, 1):
             for kk in range(ii + 1, aa_columns, 1):
                 sol[ii, :] = sol[ii, :] - sol[ii, kk] * sol[kk, :]
-                sol_bb = sol[:, aa_columns]
 
     else:
         print("linear dependency detected!")
-        sol_bb = None
+        sol = None
 
-    #print("\nfinal form:\n", sol)
+    print("\nfinal form:\n", sol)
 
-    return sol_bb
+    return sol
+
+#aa = np.array([[2.0, 4.0, 4.0], [5.0, 4.0, 2.0], [1.0, 2.0, -1.0]])
+#bb = np.array([1.0, 4.0, 2.0])
+#aa = np.array([[2.0, 4.0, 4.0], [1.0, 2.0, -1.0], [5.0, 4.0, 2.0]])
+#bb = np.array([1.0, 2.0, 4.0])
+#aa = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]])
+#bb = np.array([1.0, 2.0, 3.0])
+
+#Read matrices from file "input.in":
+file = open("input.in", "r")
+lines = file.readlines()
+#print("number of lines read:\n",lines)
+numberOfVars = int(lines[0].strip())
+aa = np.empty((numberOfVars, numberOfVars))
+bb_ma = np.empty((1,numberOfVars))
+
+#Fill Matrix bb_ma with Numbers from input.in:
+lines_bb = lines[4].strip().split()
+for jj in range(0, numberOfVars, 1):
+        bb_ma[0, jj] = float(lines_bb[jj])
+bb = bb_ma[0, :]
+
+#Fill Matrix aa with Numbers from input.in:
+for ii in range(1, numberOfVars +1, 1):
+    for jj in range(0, numberOfVars, 1):
+        aa[ii -1, jj] = float(lines[ii].strip().split()[jj])
+
+print("matrix aa:\n", aa)
+print("\nmatrix bb:\n", bb, "\n")
+
+#Solve linear equations system with method "gaussian_eliminate":
+sol = gaussian_eliminate(aa, bb)
+
+#Write obtained solution into file "output.out":
+np.savetxt("output.out", sol)
+
+#Compare with solution according numpy:
+sol_numpy = np.linalg.solve(aa, bb)
+print("\nnumpy solution:\n", sol_numpy)
